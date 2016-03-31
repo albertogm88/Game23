08d929c2d78a81208dfbbd5554bbaf03f0f672b0
Alberto <albertogm88@gmail.com>
2016-03-31 21:29:42 +0200

    Initial Commit

---
+++ Game23/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Game23
+//
+//  Created by Alberto Garcia Mendez on 31/3/16.
+//  Copyright © 2016 Alberto Garcia Mendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
